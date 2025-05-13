cb0ae8adace28701e9af65cec4c4236021230b17
WitchDevelops <d.n.wojewska@gmail.com>
2025-05-13 20:05:54 +0200

    serve static file, serve json

--- myApp.js
+++ myApp.js
@@ -1,39 +1,21 @@
-let express = require('express');
+require("dotenv").config();
+
+let express = require("express");
 let app = express();
 
+app.use("/public", express.static(__dirname + "/public"));
 
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
+app.get("/json", function (req, res) {
+  let style = process.env.MESSAGE_STYLE;
+  if (style === "uppercase") {
+    res.json({ message: "HELLO JSON" });
+  } else {
+    res.json({ message: "Hello json" });
+  }
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+exports = module.exports = app;
