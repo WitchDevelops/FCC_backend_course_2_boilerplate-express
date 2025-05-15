b62584cbb680b0b44441c4d30d8c9cff8788fddb
WitchDevelops <d.n.wojewska@gmail.com>
2025-05-15 08:23:22 +0200

    feat: time server, echo server

--- myApp.js
+++ myApp.js
@@ -3,12 +3,21 @@ require("dotenv").config();
 let express = require("express");
 let app = express();
 
+// serve static files
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+
+app.use(function middleware(req, res, next) {
+  let visitedPathDetails = req.method + " " + req.path + " - " + req.ip;
+  console.log(visitedPathDetails);
+  next();
+});
+
+//json server
 app.get("/json", function (req, res) {
   let style = process.env.MESSAGE_STYLE;
   if (style === "uppercase") {
@@ -16,6 +25,23 @@ app.get("/json", function (req, res) {
   } else {
     res.json({ message: "Hello json" });
   }
+});
+
+// time server
+app.get(
+  "/now",
+  function (req, res, next) {
+    req.time = new Date().toString();
+    next();
+  },
+  function (req, res) {
+    res.json({ time: req.time });
+  }
+);
+
+//echo server
+app.get("/:word/echo", function (req, res) {
+  res.json({ echo: req.params.word });
 });
 
 exports = module.exports = app;
